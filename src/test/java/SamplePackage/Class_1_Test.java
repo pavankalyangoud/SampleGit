35d9e6aebf2d22f30093e7aac718470367f22984
pavan <pavankalyan0795@gmail.com>
2020-03-22 16:29:28 +0530

    commit from Y person

--- src/test/java/SamplePackage/Class_1_Test.java
+++ src/test/java/SamplePackage/Class_1_Test.java
@@ -7,6 +7,8 @@ public class Class_1_Test {
 	@Test
 	public void class_1() {
 		System.out.println("Class_1 executed");
+		System.out.println("modify 1");
+		System.out.println("modify 2");
 	}
 
 }
