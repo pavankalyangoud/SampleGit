6de3e3b28aa5b70ebde7b1e482f56d0a0067ae65
pavan <pavankalyan0795@gmail.com>
2020-03-22 14:14:57 +0530

    initial commit

---
+++ src/test/java/SamplePackage/Class_1_Test.java
@@ -0,0 +1,12 @@
+package SamplePackage;
+
+import org.testng.annotations.Test;
+
+public class Class_1_Test {
+	
+	@Test
+	public void class_1() {
+		System.out.println("Class_1 executed");
+	}
+
+}
---
+++ src/test/java/SamplePackage/Class_2_Test.java
@@ -0,0 +1,11 @@
+package SamplePackage;
+
+import org.testng.annotations.Test;
+
+public class Class_2_Test {
+
+	@Test
+	public void class_2() {
+		System.out.println("Class_2 executed");
+	}
+}
